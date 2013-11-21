0a28a41e13940d6808b4268e1210d133e383dacb
Claynon Souza <claynon@gmail.com>
2013-11-21 19:50:17 +0100

    Solving problem with no argument case of unmarshalling

--- src/common/messages/KVQuery.java
+++ src/common/messages/KVQuery.java
@@ -32,6 +32,8 @@ public class KVQuery {
 		String message;
 
 		message = new String(bytes);
+		// removing /r of the end. Neccessary for setType() of no argument case.
+		message = message.substring(0, message.length() - 1);
 		arguments = message.split("\n");
 
 		if (arguments.length >= 1 && arguments.length <= 3 && bytes.length <= DROP_SIZE) {
