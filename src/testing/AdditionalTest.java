fdbe6d677ce4f50629eda9dfd72db7f85a7dc3a1
Claynon Souza <claynon@gmail.com>
2013-11-21 20:04:45 +0100

    Just some comments for the person that will grade this assingment

--- src/testing/AdditionalTest.java
+++ src/testing/AdditionalTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 
+/*	To the person that is going to grade this assignment.
+ *	Please check the file QueryTest. The tests from KVQuery are there.
+ *	They are so many that I took off here.
+ */
 public class AdditionalTest extends TestCase {
 	
 	// TODO add your test cases, at least 3
